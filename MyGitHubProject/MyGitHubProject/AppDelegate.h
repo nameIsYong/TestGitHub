1fd71bb6cd257df9c1074a20a0514da7e2175ef3
nameIsYong <1228359686@qq.com>
2017-01-14 14:50:55 +0800

    第一次提交

---
+++ MyGitHubProject/MyGitHubProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGitHubProject
+//
+//  Created by administrator on 17/1/14.
+//  Copyright © 2017年 ydl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyGitHubProject/MyGitHubProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGitHubProject
+//
+//  Created by administrator on 17/1/14.
+//  Copyright © 2017年 ydl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
